e0ba446c34ba09426894bde78b2f579a7c48b77c
Carlos Saito <carlos@saito.style>
2019-09-17 09:36:01 +0200

    Setup sync

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+require('dotenv').config()
+const skog = require('skog/bunyan')
 const server = require('./server')
+const cron = require('./cron')
+
+skog.createLogger({
+  name: 'lms-sis-monitor',
+  level: 'info'
+})
+
+cron.start()
 server.listen(3000)
